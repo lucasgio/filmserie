55a2b5d7bef81a52859cefadae3e63d4c65bd55e
lucasgio <mikaelosoltes@gmail.com>
2022-04-29 13:45:38 +0200

    Add:composition api

--- src/main.js
+++ src/main.js
@@ -7,6 +7,7 @@ import vuetify from './plugins/vuetify'
 import '@babel/polyfill'
 import 'roboto-fontface/css/roboto/roboto-fontface.css'
 import '@fortawesome/fontawesome-free/css/all.css'
+import VueCompositionAPI from '@vue/composition-api'
 
 Vue.config.productionTip = false
 
@@ -14,5 +15,7 @@ new Vue({
   router,
   store,
   vuetify,
+  VueCompositionAPI,
+
   render: h => h(App)
 }).$mount('#app')
